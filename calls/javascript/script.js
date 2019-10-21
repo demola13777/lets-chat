0985363470cce0d145414be098fa642713603b3c
demola <ademolaa640@gmail.com>
2019-10-22 01:18:27 +0200

    first commit

---
+++ calls/javascript/script.js
@@ -0,0 +1,29 @@
+
+function myFunction(){
+                document.getElementById("myDropdown").classList.
+                toggle("show");
+            }
+            //close the dropdown if the user clicks outside of it 
+            window.onClick = function(event)
+            {
+                if 
+                    (!event.target.matches('.dropbtn'))
+                    {
+                    var dropdowns =
+                        document.getElementsByClassName("dropdown-content");
+                        var i;
+                        for(i = 0;i < dropdowns.length; i++){
+                            var openDropdown = dropdowns[i];
+                            if
+                                (openDropdown.classList.contains('show'))
+{
+    openDropdown.classList.remove('show');
+                        }
+                        }
+                    }
+            }
+            
+            
+            
+            
+            
